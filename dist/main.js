d56d6db5f596db8232ba2238c060945b8a37992c
Sushant <vettel@pop-os.localdomain>
2020-04-24 20:47:56 +0530

    working on aos 23:15

--- dist/main.js
+++ dist/main.js
@@ -3,4 +3,34 @@ $(document).ready(function () {
     $(this).toggleClass("open");
     $(".top-nav").toggleClass("open");
   });
+
+  $(".top-nav .nav-link").on("click", function () {
+    $(".menu-toggler").toggleClass("open");
+    $(".top-nav").removeClass("open");
+  });
+
+  $('nav a[href*="#"]').on("click", function () {
+    $("html,body").animate(
+      (keyframes = {
+        scrollTop: $($(this).attr("href")).offset().top - 100,
+      }),
+      (options = 2000)
+    );
+  });
+
+  $("#up").on("click", function () {
+    $("html, body").animate(
+      {
+        scrollTop: 0,
+      },
+      1000
+    );
+  });
+
+  //animations
+  AOS.init({
+    easing: "ease",
+    duration: 1200,
+    once: true,
+  });
 });
